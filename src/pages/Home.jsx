075052775ceaf238db1547f706806def1900835a
musab702 <musabshaikh702@gmail.com>
2025-08-22 16:40:08 -0400

    final push?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,30 +5,32 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
-// import { ContactSection } from "../components/ContactSection";
-// import { Footer } from "../components/Footer";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
       <ThemeToggle />
+
       {/* Background Effects */}
       <StarBackground />
 
       {/* Navbar */}
       <Navbar />
+
       {/* Main Content */}
       <main>
         <HeroSection />
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
-        {/* <ContactSection /> */}
+        <ContactSection />
       </main>
 
       {/* Footer */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
