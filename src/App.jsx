42aad2ee55fbc8818fce7101f73dbf92e23d9a3e
musab702 <musabshaikh702@gmail.com>
2025-09-14 01:32:59 -0400

    added analytics, version 5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </>
   );
   
