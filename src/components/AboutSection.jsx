59fb068a87d33b906a227780e37c3a4c2945f8bf
musab702 <musabshaikh702@gmail.com>
2026-03-13 00:49:15 -0400

    added new CV again

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/assets/documents/cv.pdf"
+                href="/assets/documents/Musab_Shaikh.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
